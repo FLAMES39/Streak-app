e5a579987726fcd598d28cdfc3b53b325bd0ca67
FLAMES39 <114187269+FLAMES39@users.noreply.github.com>
2023-04-28 11:59:44 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+class Streak {
+    startDate: Date;
+    endDate: Date;
+    progress: number;
+  
+    constructor(startDate: Date) {
+      this.startDate = startDate;
+      this.endDate = Date;
+      this.progress = 0;
+    }
+  
+    updateProgress() {
+      // Logic for updating progress
+    }
+  
+    calculateStreakLength() {
+      // Logic for calculating streak length
+    }
+  }
+
+
+
+  interface StreakData {
+    id: number;
+    startDate: string;
+    endDate: string;
+    progress: number;
+  }
+  
